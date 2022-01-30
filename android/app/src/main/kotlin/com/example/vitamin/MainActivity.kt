bc6bd9d7c99e71a8298722b893716a302a1fb9bf
Mohammadshahsamirbhu <samir_reaz@yahoo.com>
2022-01-31 05:57:34 +0600

    init project

---
+++ android/app/src/main/kotlin/com/example/vitamin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.vitamin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/home/home_screen.dart
@@ -0,0 +1,12 @@
+// it's our home page.
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
