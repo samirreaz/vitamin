9441e3c2d7148a28bf494885bd8ac4294a2e4155
ABDUS_SAMAD_RIFAT AbdusSamad95 <mdsamad041@gmail.com>
2022-01-31 23:41:30 +0600

    all

--- lib/models/vitamin_about_model.dart
+++ lib/models/vitamin_about_model.dart
@@ -8,7 +8,7 @@ class VitaminAboutModel {
     required this.vitaminName,
     required this.fruitImg,
     required this.description,
-    required this.graphImg,
+    required this.graphImg, https,
   });
 }
 
@@ -16,24 +16,35 @@ class VitaminAboutModel {
 List<VitaminAboutModel> vitaminAboutList = [
   VitaminAboutModel(
       vitaminName: 'Vitamin A',
-      fruitImg: 'assets/images/1.jpg',
-      description: 'description',
-      graphImg: 'graphImg'),
+      fruitImg: 'https://media.istockphoto.com/photos/food-sources-of-beta-carotene-and-vitamin-a-picture-id511051020',
+      description: '',
+      graphImg: 'https://images-prod.healthline.com/hlcmsresource/images/AN_images/node_128741_graphs/01-vitamin-a-chart-graph.jpg'),
   VitaminAboutModel(
-      vitaminName: 'Vitamin A',
-      fruitImg: '',
-      description: 'description',
-      graphImg: 'graphImg'),
+      vitaminName: 'Vitamin B',
+      fruitImg: 'https://media.istockphoto.com/photos/high-vitamin-b-sources-assortment-picture-id1256627955?s=612x612',
+      description: '',
+      graphImg: 'https://images-prod.healthline.com/hlcmsresource/images/AN_images/node_128741_graphs/03-vitamin-e-chart-graph.jpg'),
   VitaminAboutModel(
-    vitaminName: 'Vitamin A',
-    fruitImg: '',
-    description: 'description',
-    graphImg: 'graphImg',
+    vitaminName: 'Vitamin C',
+    fruitImg: 'https://www.bing.com/images/blob?bcid=S6GddBqUl8QD88MKZWoDXW029YjA.....6k',
+    description: ''
+',graphImg: 'https://images-prod.healthline.com/hlcmsresource/images/AN_images/node_128741_graphs/03-vitamin-e-chart-graph.jpg',
+  , graphImg: ''),
+  VitaminAboutModel(
+    vitaminName: 'Vitamin D',
+    fruitImg: 'https://media.istockphoto.com/photos/healthy-foods-containing-vitamin-d-picture-id997087146',
+    description: '',
+    graphImg: 'https://images-prod.healthline.com/hlcmsresource/images/AN_images/node_128741_graphs/02-vitamin-d-chart-graph.jpg',
   ),
   VitaminAboutModel(
-    vitaminName: 'Vitamin z',
-    fruitImg: '',
-    description: 'description',
-    graphImg: 'graphImg',
+    vitaminName: 'Vitamin E',
+    fruitImg: 'https://media.istockphoto.com/photos/assortment-food-sources-of-vitamin-e-picture-id1080275050?s=612x612',
+    description: '',
+    graphImg: 'https://images-prod.healthline.com/hlcmsresource/images/AN_images/node_128741_graphs/03-vitamin-e-chart-graph.jpg',
+  ),VitaminAboutModel(
+    vitaminName: 'Vitamin K',
+    fruitImg: 'https://media.istockphoto.com/photos/fruits-and-vegetables-containing-vitamin-k-minerals-and-dietary-fiber-picture-id967577294?s=612x612',
+    description: '',
+    graphImg: 'https://images-prod.healthline.com/hlcmsresource/images/AN_images/node_128741_graphs/04-vitamin-k-chart-graph.jpg',
   ),
 ];
