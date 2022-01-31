3b3cdfddc920740dce54c15b0504a2944a113d05
abirhasnat95 <abirhasnat@2016@gmail.com>
2022-01-31 14:28:47 +0600

    Appbar and body was added

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -7,6 +7,33 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Vitamin'),
+      ),
+      backgroundColor: Colors.grey[300],
+      body: Column(
+        children: [
+          Center(
+            child: Text(
+              'Vitamin List',
+              style: TextStyle(
+                  color: Colors.amber,
+                  fontSize: 30,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          Column(children: [
+            Container(
+              child: Text(
+                'রিফাত ভিটামিন শূন্যতায় ভুগছে',
+                style: TextStyle(fontSize: 30),
+              ),
+            ),
+          ])
+        ],
+      ),
+    );
   }
 }
