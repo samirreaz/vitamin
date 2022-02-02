b24df7b5f8d8511b1bc386054baa7e4f4f5eb94b
Mohammadshahsamirbhu <samir_reaz@yahoo.com>
2022-02-02 09:34:13 +0600

    home screen mocup design

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -1,18 +1,27 @@
 // it's our home page.
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:vitamin/models/vitamin_category_model.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Size _size = MediaQuery.of(context).size;
     return Scaffold(
+      backgroundColor: Colors.white.withOpacity(.8),
       body: ListView.builder(
-        itemCount: 4,
+        itemCount: vitamins.length,
         itemBuilder: (context, index) => Container(
-          height: 200,
-          color: Colors.black,
+          margin: EdgeInsets.all(10),
+          height: _size.height * 0.25,
+          color: Colors.white,
+          child: Image(
+            image: NetworkImage(vitamins[index].vitaminImg),
+            fit: BoxFit.scaleDown,
+          ),
         ),
       ),
     );
